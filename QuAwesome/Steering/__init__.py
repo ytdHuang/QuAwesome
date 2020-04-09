f5e8570e975b7b233047bf54e668f68e49f8753c
Y.T. Huang <y.t.d.huang@phys.ncku.edu.tw>
2020-04-09 21:41:12 +0800

    Generalize arbitrary assemblage input for Steering SDP

--- QuAwesome/Steering/__init__.py
+++ QuAwesome/Steering/__init__.py
@@ -1,4 +1,2 @@
-from QuAwesome.Steering.TSR import TSR
-from QuAwesome.Steering.TSW import TSW
-from QuAwesome.Steering.SSR import SSR
-from QuAwesome.Steering.SSW import SSW
+from QuAwesome.Steering.Weight import steeringWeight as Weight
+from QuAwesome.Steering.Robustness import steeringRobustness as Robustness
